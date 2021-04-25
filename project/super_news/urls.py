8fc9b5150e38934a9a5fcaaa1339cd86ad26a061
barkoig <kbarbolin@gmail.com>
2021-04-25 20:57:13 +0300

    polls / my templates / staticfiles

--- project/super_news/urls.py
+++ project/super_news/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from polls.views import index, detail, blog_hendler
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-]
+    path('', index),
+    path('blog/', blog_hendler),
+    path('polls/<int:question_id>/', detail),
+    path('admin/', admin.site.urls)
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
